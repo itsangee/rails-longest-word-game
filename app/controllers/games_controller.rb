8f8ad353402d69e0615175f00304142cf6484d54
angelee otic <otic.angeleemae@gmail.com>
2022-10-14 19:15:31 +0100

    with logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,29 @@
+require "open-uri"
+
 class GamesController < ApplicationController
 
-  def score
-    # params access then
-    # compare the letters
-    # check with api
-  end
+  VOWELS = %w(A E I O U Y)
 
   def new
-    @letters = ("a".."z").to_a.min(10).shuffle
+    @letters = ('a'..'z').to_a.min(10).shuffle
+  end
 
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @include = include?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def include?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
